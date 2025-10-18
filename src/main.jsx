cd015f14838e82353b12551f223f0e62ac8676f3
Ishu <ishukashyap290@gmail.com>
2025-10-18 13:04:48 +0530

    Fix import path for Context

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { StrictMode } from 'react'
-import Context from './context/context.jsx'
+import Context from './context/Context.jsx'
 
 createRoot(document.getElementById('root')).render(
   <Context>
